fc9102559838e1403c271ef5f914c7cdca735ee3
Mehmet OGUT <mogut44949@gmail.com>
2024-05-13 12:00:32 +0300

    ReChange

--- src/useState/UseStateObject.jsx
+++ src/useState/UseStateObject.jsx
@@ -14,6 +14,36 @@ const UseStateObject = () => {
         age : 24,
     })
 
+    //let toggle = false;
+    const [toggle,setToggle] = useState(false);
+
+    const handleReChange = ()=>{
+        //toggle = !toggle
+
+        setToggle(!toggle )
+
+        console.log(toggle) // bunu dışa yazarsan sadece ilk değerini yazar ve bir daha render edilmez
+
+        if(toggle){
+            setPerson(
+                ({
+                    name: "Hilal",
+                    jobs:  "MBG",
+                    age : 24,
+                })
+            )
+
+        }else {
+            setPerson(
+                ({
+                    name: "Mehmet",
+                    jobs:  "Developer",
+                    age : 26,
+                })
+            )
+
+        }
+    }
 
     const {name, jobs, age} = person; // state destructure yapıldı
   return (
@@ -23,9 +53,10 @@ const UseStateObject = () => {
         <h3>Jobs: {jobs}</h3>
         <h4>Age:  {age}</h4>
 
-        <button onClick={()=>setPerson({...person, name: "Mehmet", jobs: "Developer" ,age: age+1})}>Change Name</button>
+        <button onClick={()=>setPerson({...person, name: "Mehmet", jobs: "Developer" ,age: age+1})}>Change </button>
         {/* <button onClick={()=>setJobs("CTO")}>Change Name</button>
         <button onClick={()=>setAge(29)}>Change Age</button> */}
+        <button onClick={handleReChange}>ReChange</button>
     </div>
   )
 }
