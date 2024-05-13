cf880cfe30e414daa55e2bc7c55ac7db01649967
Mehmet OGUT <mogut44949@gmail.com>
2024-05-13 10:15:47 +0300

    INC-DEC-CLR COUNT

--- src/useState/UseStateCounter.jsx
+++ src/useState/UseStateCounter.jsx
@@ -24,8 +24,11 @@ export const UseStateCounter = () => {
         <h1>UseState Counter</h1>
         <h2> Count:{count}</h2>
         <button onClick={handleCount}>INC</button>
-        <button>DEC</button>
-        <button>CLR</button>
+        {/*Eğer setCount metodunu callback(arow fonc) ile yazmasak bu metot ilk açılıştan itibaren doğrudan çalışmış olur
+           Bu durumda da count state'ini günceller , State güncellendiği içinde componenet re-render olur 
+           Re-render ise setCount araccılığı ile state'in güncellenmesine yol açar ve sonsuz döngüye girer*/}
+        <button onClick={()=>{count>0 &&   setCount(count-1)}}>DEC</button>
+        <button onClick={()=>{setCount(0 )}}>CLR</button>
 
     </div>
   )
